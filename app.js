99884631ff1d1a5c567a172b6bde8840a5f117a4
Krishna P.. Kundu <krishnaprasadkundu2580@gmail.com>
2026-08-28 21:59:11 +0530

    new

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use('/',userAuthRoute)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* Server */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
